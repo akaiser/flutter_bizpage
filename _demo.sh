6d2890d9436a13b192bc6b5986e517dfe020067e
Albert Kaiser <ak@kaiserv.de>
2021-08-26 16:33:52 +0200

    add web stuff

---
+++ _demo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -r _demo/apk/*
+rm -r _demo/canvaskit/*
+rm -r _demo/html/*
+flutter clean
+
+flutter build apk --split-per-abi
+cp build/app/outputs/apk/release/app-* _demo/apk/
+
+flutter build web --web-renderer canvaskit
+cp -r build/web/* _demo/canvaskit
+sed -i "" 's|\="/"|\="/flutter_bizpage/canvaskit/"|' _demo/canvaskit/index.html
+
+flutter build web --web-renderer html
+cp -r build/web/* _demo/html
+sed -i "" 's|\="/"|\="/flutter_bizpage/html/"|' _demo/html/index.html
+
+# git subtree push --prefix _demo origin gh-pages
