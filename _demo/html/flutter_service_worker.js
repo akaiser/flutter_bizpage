6bfe69fd4b75ce54003101382bdf1916015b6add
Albert Kaiser <ak@kaiserv.de>
2021-09-13 01:01:56 +0200

    update demo

--- _demo/html/flutter_service_worker.js
+++ _demo/html/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "28e187824207f685344033b211eb7d84",
+  "version.json": "31517a792167f364d3297a60d3f69306",
 "favicon.ico": "1e1019dc22fac612a61ab71f2fdffd6d",
-"index.html": "02642f2b1ac96b018b99a3851f7c0efb",
-"/": "02642f2b1ac96b018b99a3851f7c0efb",
-"main.dart.js": "3515c7f243f2f7e513c544dcfa445993",
+"index.html": "178867dbc9804b5396e395cd42927f32",
+"/": "178867dbc9804b5396e395cd42927f32",
+"main.dart.js": "86808acee8bb0134d8fef9408f34106a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "8596448c84f4a1e2c172943304c3f080",
